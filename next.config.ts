0086453f1782fd30445c3eb2a091eabfa445e66e
kiritocode1 <kathawalearyan9@gmail.com>
2025-08-19 12:59:09 +0530

    ANY is bad

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	module: {
-		rules: [
-			{
-				test: /\.mp3$/,
-        use: [ 
-          {
-            loading:"file-loader"
-          }
-        ]
-			},
-		],
-	},
 };
 
 export default nextConfig;
